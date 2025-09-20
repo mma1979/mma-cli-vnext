d591d31a89c56dc0ca642b360a23ce6a6b62acfd
Mohammed Abdelhay <mamado2000@gmail.com>
2025-09-20 23:24:53 +0300

    start new UI

---
+++ cli-app/Mma.Cli/Mma.Cli.UI/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Mma.Cli.UI.Models;
+
+using System.Diagnostics;
+
+namespace Mma.Cli.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+               
+    }
+}
